c67d86cd8a1edb3474ee7fe65f3ff95f09566873
Henry Ing-Simmons <hing-simmons@aquilalearning.com>
2023-10-05 17:24:30 +0100

    Update vue tsx rules (#21)

--- packages/integration-vue/tests/component.tsx
+++ packages/integration-vue/tests/component.tsx
@@ -1,5 +1,6 @@
 import { Vue } from "vue-property-decorator";
 import type { VNode } from "vue";
+
 export default class TestComponent extends Vue {
 
     public render(): VNode {
@@ -8,6 +9,15 @@ export default class TestComponent extends Vue {
                 <i class="good self closing i tag" />
                 {/* eslint-disable-next-line react/self-closing-comp */}
                 <i class="bad non self closing i tag"></i>
+
+                <good-comp-many-props
+                    foo="abc"
+                    bar="def"
+                    fizz="ghi"
+                />
+
+                {/* eslint-disable-next-line react/jsx-max-props-per-line */}
+                <bad-comp-many-props foo="abc" bar="def" fizz="ghi" />
             </div>
         );
     }
