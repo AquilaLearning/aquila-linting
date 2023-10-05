eab05b9da46aae10a5037b6396e10cd1215a0475
Henry Ing-Simmons <hing-simmons@aquilalearning.com>
2023-10-05 17:52:00 +0100

    Add rule for jsx-wrap-multilines

--- packages/integration-vue/tests/component.tsx
+++ packages/integration-vue/tests/component.tsx
@@ -1,9 +1,27 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Vue } from "vue-property-decorator";
 import type { VNode } from "vue";
 
 export default class TestComponent extends Vue {
 
     public render(): VNode {
+
+        const goodDeclaration = (
+            <div />
+        );
+
+        const goodSingleLineDeclaration = <div />;
+
+        // eslint-disable-next-line react/jsx-wrap-multilines
+        const badDeclaration = <div>
+                Hello world
+        </div>;
+
+        // eslint-disable-next-line react/jsx-wrap-multilines
+        const badDeclarationParens = (<div>
+            Hello world
+        </div>);
+
         return (
             <div>
                 <i class="good self closing i tag" />
@@ -18,6 +36,17 @@ export default class TestComponent extends Vue {
 
                 {/* eslint-disable-next-line react/jsx-max-props-per-line */}
                 <bad-comp-many-props foo="abc" bar="def" fizz="ghi" />
+
+                {true && (
+                    <div>
+                        good-wrapped-comp
+                    </div>
+                )}
+
+                {/* eslint-disable-next-line react/jsx-wrap-multilines */}
+                {false && <div>
+                    Bad wrapped component
+                </div>}
             </div>
         );
     }
